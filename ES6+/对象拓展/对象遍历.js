c18171587e541a374fe0b643095ae0041fba8b4c
wang <2531639447@qq.com>
2024-09-04 14:56:49 +0800

    更新

--- ES6+/对象拓展/对象遍历.js
+++ ES6+/对象拓展/对象遍历.js
@@ -4,6 +4,7 @@ let obj={
     skill:'中国武术',
 }
 
+Object.defineProperty(obj,'skill',{enumerable:false,})     //此时skill不能被打印
 console.log(Object.keys(obj))
 console.log(Object.values(obj))
 console.log(Object.entries(obj))
